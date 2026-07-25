adac9ed248f2f635241c4933904fc0177f5adb61
ShreyashiGupta2310 <shreyashigupta2310@gmail.com>
2026-07-26 00:10:46 +0530

    feat: add getCommits and groupCommitsByDate to fetch and summarize commit activity

--- app/health/page.tsx
+++ app/health/page.tsx
@@ -1,5 +1,7 @@
 import { GetRepoInfo } from "@/lib/github";
 import { GetRepoReadme } from "@/lib/github";
+import { GetRepoCommits } from "@/lib/github";
+import { GetCommitByDate } from "@/lib/github";
 
 export default async function HealthPage() {
   // const data = await GetRepoInfo("ShreyashiGupta2310", "GitVyuh_v0");
@@ -14,12 +16,33 @@ export default async function HealthPage() {
   //   </div>
   // );
 
-  const readme = await  GetRepoReadme("ShreyashiGupta2310", "GitVyuh_v0");
+  // const readme = await  GetRepoReadme("ShreyashiGupta2310", "GitVyuh_v0");
 
+  // return (
+  //   <div>
+  //     <h1>Health Check</h1>
+  //     <pre>{readme}</pre>
+  //   </div>
+  // );
+
+
+
+  // I should put some logic to get a clear and concise health check of commits
+  // const commits = await GetRepoCommits("ShreyashiGupta2310", "GitVyuh_v0");
+  // return (
+  //   <div>
+  //     <h1>Health Check</h1>
+  //     <pre>{JSON.stringify(commits, null, 2)}</pre>
+  //   </div>
+  // );
+
+  const commits = await GetRepoCommits("ShreyashiGupta2310", "GitVyuh_v0");
+  
+  const commitByDate = await  GetCommitByDate(commits);
   return (
     <div>
       <h1>Health Check</h1>
-      <pre>{readme}</pre>
+      <pre>{JSON.stringify(commitByDate, null, 2)}</pre>
     </div>
   );
 }
--- lib/github.ts
+++ lib/github.ts
@@ -22,3 +22,24 @@ export async function GetRepoReadme(owner:string, repo:string) {
   
     return decodedContent;
 }
+export async function GetRepoCommits(owner:string, repo:string) {
+    const response = await fetch(`https://api.github.com/repos/${owner}/${repo}/commits`,{
+        headers: {
+            'Authorization': `Bearer ${process.env.GITHUB_TOKEN}`
+        },
+    
+    });
+    const data = await response.json();
+    return data;
+
+}
+//Logic 
+export async function GetCommitByDate(commits : any[]) {
+    const count :Record<string, number> = {};
+    for (const commit of commits) {
+       const date = new Date(commit.commit.author.date).toISOString().split('T')[0];
+       count[date] = (count[date] || 0) + 1;
+    }
+    return Object.entries(count).map(([date, count]) => ({ date, count }));
+
+}
