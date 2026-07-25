5edd140bc95f79be790c1ed61aa64a51aec4f754
ShreyashiGupta2310 <shreyashigupta2310@gmail.com>
2026-07-25 20:18:13 +0530

    feat: add getReadme function to fetch and decode repo README content

--- app/health/page.tsx
+++ app/health/page.tsx
@@ -1,15 +1,25 @@
 import { GetRepoInfo } from "@/lib/github";
+import { GetRepoReadme } from "@/lib/github";
 
 export default async function HealthPage() {
-  const data = await GetRepoInfo("ShreyashiGupta2310", "GitVyuh_v0");
+  // const data = await GetRepoInfo("ShreyashiGupta2310", "GitVyuh_v0");
+
+  // return (
+  //   <div>
+      
+  //     <h1>Health Check</h1>
+  //     <p>Repo name: {data.name}</p>
+  //     <p>Stars: {data.stargazers_count}</p>
+  //     <p>Description: {data.description}</p>
+  //   </div>
+  // );
+
+  const readme = await  GetRepoReadme("ShreyashiGupta2310", "GitVyuh_v0");
 
   return (
     <div>
-      
       <h1>Health Check</h1>
-      <p>Repo name: {data.name}</p>
-      <p>Stars: {data.stargazers_count}</p>
-      <p>Description: {data.description}</p>
+      <pre>{readme}</pre>
     </div>
   );
 }
--- lib/github.ts
+++ lib/github.ts
@@ -9,3 +9,16 @@ export async function GetRepoInfo(owner:string, repo:string) {
     const data= await response.json();
     return data;
 }
+
+export async function GetRepoReadme(owner:string, repo:string) {
+    const response = await fetch(`https://api.github.com/repos/${owner}/${repo}/readme`,{
+        headers: {
+            'Authorization': `Bearer ${process.env.GITHUB_TOKEN}`
+        },
+     
+    });
+    const data = await response.json();
+    const decodedContent = Buffer.from(data.content, "base64").toString("utf-8");
+  
+    return decodedContent;
+}
