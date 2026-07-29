c3ff2cd5836523e8dea3d86032f5ee6c1e1d34d2
ShreyashiGupta2310 <shreyashigupta2310@gmail.com>
2026-07-29 16:58:33 +0530

    feat: combine all 4 GitHub data sources into one /api/github response using Promise.all

--- app/api/github/route.ts
+++ app/api/github/route.ts
@@ -1,6 +1,7 @@
 import { error } from "console";
 import { NextRequest,NextResponse } from "next/server";
-import { GetRepoInfo } from "@/lib/github";
+import { GetRepoInfo ,GetRepoReadme,GetRepoCommits,GetFolderStructure, buildFolderTree ,GetCommitByDate} from "@/lib/github";
+import next from "next";
 
 export async function GET (request :NextRequest){
 const SearchParams = request.nextUrl.searchParams;
@@ -11,7 +12,22 @@ if(!owner || !repo){
     return NextResponse.json( {error:"Missing owner or repo"},{status:400});
 }
 
-const repoInfo = await GetRepoInfo(owner, repo);
+// const repoInfo = await GetRepoInfo(owner, repo);
 
-return NextResponse.json(repoInfo);
+// return NextResponse.json(repoInfo);
+
+const [repoInfo , readme , commits , flodertree]= await Promise.all([
+    GetRepoInfo(owner,repo),
+    GetRepoReadme(owner, repo),
+    GetRepoCommits(owner,repo),
+    GetFolderStructure(owner,repo)
+]);
+const groupALLcommits= await GetCommitByDate(commits);
+const folderTree=await buildFolderTree(flodertree);
+return NextResponse.json({
+    repoInfo ,
+     readme , 
+     commits:groupALLcommits ,
+      folderTree
+});
 }
